dedbc63e28a401be328d5e5a1ea21f95e4389eee
Yves Bastide <yves@botify.com>
2017-11-04 13:41:54 +0100

    Works with moto 1.0.x
    Try importing mock_*_deprecated, the moto 1.0.x decorators for boto2.
    See [Using moto 1.0.X with boto2](https://github.com/spulec/moto#using-moto-10x-with-boto2).
    
    Signed-off-by: Yves Bastide <yves@botify.com>

--- tests/test_simpleflow/test_storage.py
+++ tests/test_simpleflow/test_storage.py
@@ -2,7 +2,11 @@ import os
 import unittest
 import tempfile
 import boto
-from moto import mock_s3
+
+try:
+    from moto import mock_s3_deprecated as mock_s3
+except ImportError:
+    from moto import mock_s3
 
 from simpleflow import storage, settings
 
